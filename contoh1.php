e626d460495a0a2eb7e57c0f03ab83aab21a45f6
farellg <145657051+farellg@users.noreply.github.com>
2023-10-27 16:03:50 +0700

    Tugas Pertemuan 1
    Materi Pertemuan 2

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Al Farell Ghazali<br>
+        saya tinggal di daerah Depok<br>
+        olahraga yang saya sukai adalah Basket";
+    }
+}
